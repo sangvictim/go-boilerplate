927b792268ffb7ffb048b82befe9eee599c8a322
Tio <sangvictim@gmail.com>
2025-02-23 18:23:41 +0700

    chore: update go version

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"context"
 	"fmt"
 	"go-boilerplate/config"
-	"io"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	S3Config "github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
-	S3 "github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/gofiber/fiber/v2"
 )
 
 // main initializes the application by loading configuration, setting up logging,
@@ -35,55 +25,6 @@ func main() {
 		Log:      log,
 		Config:   viperConfig,
 		S3:       s3,
-	})
-
-	app.Post("/test", func(c *fiber.Ctx) error {
-
-		var (
-			accessKey = viperConfig.GetString("s3.access_key")
-			secretKey = viperConfig.GetString("s3.secret_key")
-			endpoint  = viperConfig.GetString("s3.endpoint")
-			region    = viperConfig.GetString("s3.region")
-		)
-
-		cfg, err := S3Config.LoadDefaultConfig(context.TODO())
-		if err != nil {
-			log.Fatalf("unable to load SDK config, %v", err)
-		}
-
-		client := S3.NewFromConfig(cfg, func(o *S3.Options) {
-			o.Region = region
-			o.UsePathStyle = true
-			o.EndpointOptions.DisableHTTPS = true
-			o.BaseEndpoint = aws.String(endpoint)
-			o.Credentials = credentials.NewStaticCredentialsProvider(accessKey, secretKey, "")
-		})
-
-		uploader := manager.NewUploader(client)
-
-		file, err := c.FormFile("avatar")
-		if err != nil {
-			return fiber.NewError(fiber.StatusBadRequest, "Invalid file")
-		}
-		fileBytes, _ := file.Open()
-
-		defer fileBytes.Close()
-		path := "avatar/" + file.Filename
-		fileContent, _ := io.ReadAll(fileBytes)
-		result, err := uploader.Upload(context.TODO(), &S3.PutObjectInput{
-			Bucket: aws.String("go-boilerplate"),
-			Key:    aws.String(path),
-			Body:   bytes.NewReader(fileContent),
-		})
-
-		if err != nil {
-			log.Errorf("Failed to upload file : %+v", err.Error())
-			return err
-		}
-
-		return c.JSON(fiber.Map{
-			"message": result,
-		})
 	})
 
 	webPort := viperConfig.GetInt32("web.port")
